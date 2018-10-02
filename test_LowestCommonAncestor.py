9652b00deb5ae53db8ea6f90b7c9f68d5925d0f4
Gabriel <35768797+Gabe1704@users.noreply.github.com>
2018-10-02 14:51:52 +0100

    Add files via upload

--- test_LowestCommonAncestor.py
+++ test_LowestCommonAncestor.py
@@ -49,3 +49,17 @@ class TestLowestCommonAncestor(unittest.TestCase):
         self.assertEqual(answer, 7)
         answer = LowestCommonAncestor.LCA(root, 4, 4)
         self.assertEqual(answer, 4)
+
+# testing what happens if I just use the root
+    def test_LCARoot(self):
+
+        root = Node(1)
+        root.left = Node(2)
+        root.right = Node(3)
+        root.left.left = Node(4)
+        root.left.right = Node(5)
+        root.right.left = Node(6)
+        root.right.right = Node(7)
+
+        answer = LowestCommonAncestor.LCA(root, 1, 1)
+        self.assertEqual(answer, 1)       
