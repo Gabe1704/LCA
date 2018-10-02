f60ab3b2c10f28108e43cb8187d4ef88fcd552ee
Gabriel <35768797+Gabe1704@users.noreply.github.com>
2018-10-02 14:47:20 +0100

    Add files via upload

--- test_LowestCommonAncestor.py
+++ test_LowestCommonAncestor.py
@@ -5,8 +5,12 @@ from LowestCommonAncestor import Node
 
 class TestLowestCommonAncestor(unittest.TestCase):
 
+#                     1
+#             2               3
+#        4       5         6       7
+# look of tree
 
-
+# testing normal functionality
     def test_LCA(self):
 
         root = Node(1)
@@ -29,3 +33,19 @@ class TestLowestCommonAncestor(unittest.TestCase):
         self.assertEqual(answer, 2)
         answer = LowestCommonAncestor.LCA(root, 5, 6)
         self.assertEqual(answer, 1)
+
+# testing what happens if I just use the same node
+    def test_LCASame(self):
+
+        root = Node(1)
+        root.left = Node(2)
+        root.right = Node(3)
+        root.left.left = Node(4)
+        root.left.right = Node(5)
+        root.right.left = Node(6)
+        root.right.right = Node(7)
+
+        answer = LowestCommonAncestor.LCA(root, 7, 7)
+        self.assertEqual(answer, 7)
+        answer = LowestCommonAncestor.LCA(root, 4, 4)
+        self.assertEqual(answer, 4)
